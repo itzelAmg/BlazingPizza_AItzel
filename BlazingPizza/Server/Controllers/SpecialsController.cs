af765679db2a54587e7b432ebe930b1cd1640562
Itzel Ambrosio <Itzel.Ambrosio@Solera.com>
2023-04-03 18:32:36 -0600

    cambios en controller y program cs proyecto server

--- BlazingPizza/Server/Controllers/SpecialsController.cs
+++ BlazingPizza/Server/Controllers/SpecialsController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using BlazingPizza.Server.Models;
+using BlazingPizza.Shared;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazingPizza.Server.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("specials")]
     [ApiController]
     public class SpecialsController : ControllerBase
     {
+        private readonly PizzaStoreContext Context;
+        public SpecialsController(PizzaStoreContext context)
+        {
+            this.Context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+        {
+            return await Context.Specials
+            .OrderByDescending(s => s.BasePrice).ToListAsync();
+        }
+
+
     }
 }
--- BlazingPizza/Server/Program.cs
+++ BlazingPizza/Server/Program.cs
@@ -4,28 +4,37 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("PizzaStore");
-builder.Services.AddDbContext<PizzaStoreContext>(
-    options => options.UseSqlServer(connectionString));
-
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<PizzaStoreContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("PizzaStore")));
+
 
 var app = builder.Build();
+
+
+//var connectionString = builder.Configuration.GetConnectionString("PizzaStore");
+//builder.Services.AddDbContext<PizzaStoreContext>(
+//    options => options.UseSqlServer(connectionString));
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseWebAssemblyDebugging();
+    app.UseWebAssemblyDebugging();
 }
-//else
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+else
+{
+   app.UseExceptionHandler("/Error");
+
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
@@ -38,5 +47,16 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapControllers();
 app.MapFallbackToFile("index.html");
+
+var ScopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var Scope = ScopeFactory.CreateScope())
+{
+    var Context = Scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (!Context.Specials.Any())
+    {
+        SeedData.Initialize(Context);
+    }
+}
+
 
 app.Run();
